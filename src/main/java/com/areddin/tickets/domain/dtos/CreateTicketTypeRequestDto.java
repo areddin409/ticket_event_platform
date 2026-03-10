21a2274b58ac313973a5c28e64cc36c0ed897a1c
Andrew Reddin <areddin409@gmail.com>
2026-03-09 19:35:36 -0500

    remove unused import for jakarta.validation.Valid in CreateTicketTypeRequestDto

--- src/main/java/com/areddin/tickets/domain/dtos/CreateTicketTypeRequestDto.java
+++ src/main/java/com/areddin/tickets/domain/dtos/CreateTicketTypeRequestDto.java
@@ -1,6 +1,5 @@
 package com.areddin.tickets.domain.dtos;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
